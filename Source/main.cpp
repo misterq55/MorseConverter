ab51ff40683cf833ba5658c864af648f680fe0bf
misterq <illsi@naver.com>
2023-12-19 00:24:30 +0900

    json 변환 작업중

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include <string>
 // #include "FHangulParser.h"
 #include "MorseConverter/MorseConverter.h"
@@ -9,6 +10,273 @@
 #include "jsoncpp/json/json.h"
 
 using namespace std;
+
+void makeEnglishMorseCodeBook() {
+
+  Json::Value root;
+  Json::Value fromCode;
+  Json::Value keyValueType;
+  keyValueType["keyVariableType"] = "Int";
+  keyValueType["valueVariableType"] = "WChar";
+  fromCode["variableType"] = keyValueType;
+  fromCode["7"] = L'A';
+  fromCode["41"] = L'B';
+  fromCode["50"] = L'C';
+  fromCode["14"] = L'D';
+  fromCode["1"] = L'E';
+  fromCode["49"] = L'F';
+  fromCode["17"] = L'G';
+  fromCode["40"] = L'H';
+  fromCode["4"] = L'I';
+  fromCode["79"] = L'J';
+  fromCode["23"] = L'K';
+  fromCode["43"] = L'L';
+  fromCode["8"] = L'M';
+  fromCode["5"] = L'N';
+  fromCode["26"] = L'O';
+  fromCode["52"] = L'P';
+  fromCode["71"] = L'Q';
+  fromCode["16"] = L'R';
+  fromCode["13"] = L'S';
+  fromCode["2"] = L'T';
+  fromCode["22"] = L'U';
+  fromCode["67"] = L'V';
+  fromCode["25"] = L'W';
+  fromCode["68"] = L'X';
+  fromCode["77"] = L'Y';
+  fromCode["44"] = L'Z';
+  fromCode["241"] = L'1';
+  fromCode["238"] = L'2';
+  fromCode["229"] = L'3';
+  fromCode["202"] = L'4';
+  fromCode["121"] = L'5';
+  fromCode["122"] = L'6';
+  fromCode["125"] = L'7';
+  fromCode["134"] = L'8';
+  fromCode["161"] = L'9';
+  fromCode["242"] = L'0';
+  fromCode["355"] = L'-'; 
+  fromCode["637"] = L'.';
+  fromCode["692"] = L',';
+  fromCode["158"] = L'(';
+  fromCode["644"] = L')';
+  fromCode["400"] = L'?';
+  fromCode["149"] = L'/';
+
+  Json::Value toCode;
+
+  keyValueType["keyVariableType"] = "String";
+  keyValueType["valueVariableType"] = "String";
+  toCode["variableType"] = keyValueType;
+  toCode["A"] = ".-";
+  toCode["B"] = "-...";
+  toCode["C"] = "-.-.";
+  toCode["D"] = "-..";
+  toCode["E"] = ".";
+  toCode["F"] = "..-.";
+  toCode["G"] = "--.";
+  toCode["H"] = "....";
+  toCode["I"] = ".---";
+  toCode["J"] = ".---";
+  toCode["K"] = "-.-";
+  toCode["L"] = ".-..";
+  toCode["M"] = "--";
+  toCode["N"] = "-.";
+  toCode["O"] = "---";
+  toCode["P"] = ".--.";
+  toCode["Q"] = "--.-";
+  toCode["R"] = ".-.";
+  toCode["S"] = "...";
+  toCode["T"] = "-";
+  toCode["U"] = "..-";
+  toCode["V"] = "...-";
+  toCode["W"] = ".--";
+  toCode["X"] = "-..-";
+  toCode["Y"] = "-.--";
+  toCode["Z"] = "--..";
+  toCode["1"] = ".----";
+  toCode["2"] = "..---";
+  toCode["3"] = "...--";
+  toCode["4"] = "....-";
+  toCode["5"] = ".....";
+  toCode["6"] = "-....";
+  toCode["7"] = "--...";
+  toCode["8"] = "---..";
+  toCode["9"] = "----.";
+  toCode["0"] = "-----";
+  toCode["-"] = "-....-";
+  toCode["."] = ".-.-.-";
+  toCode[","] = "--..--";
+  toCode["("] = "-.--.";
+  toCode[")"] = "-.--.-";
+  toCode["?"] = "..--..";
+  toCode["/"] = "-..-.";
+  toCode[" "] = "/";
+
+  root["fromCode"] = fromCode;
+  root["toCode"] = toCode;
+
+  // cout << root << endl;
+  Json::StreamWriterBuilder builder;
+  const std::unique_ptr<Json::StreamWriter> writer(builder.newStreamWriter());
+  std::ofstream output_file("MorseEnglishCodeBook.json");
+  writer->write(root, &output_file);
+}
+
+void makeKoreanMorseCodeBook() 
+{
+  Json::Value root;
+  Json::Value fromCode;
+  Json::Value keyValueType;
+  keyValueType["keyVariableType"] = "Int";
+  keyValueType["valueVariableType"] = "WChar";
+  fromCode["variableType"] = keyValueType;
+  fromCode["43"] = "ぁ";
+  fromCode["49"] = "い";
+  fromCode["41"] = "ぇ";
+  fromCode["67"] = "ぉ";
+  fromCode["8"] = "け";
+  fromCode["25"] = "げ";
+  fromCode["17"] = "さ";
+  fromCode["23"] = "し";
+  fromCode["52"] = "じ";
+  fromCode["50"] = "ず";
+  fromCode["68"] = "せ";
+  fromCode["44"] = "ぜ";
+  fromCode["26"] = "そ";
+  fromCode["79"] = "ぞ";
+
+  /*fromCode["26"] = L'O';
+  fromCode["52"] = L'P';
+  fromCode["71"] = L'Q';
+  fromCode["16"] = L'R';
+  fromCode["13"] = L'S';
+  fromCode["2"] = L'T';
+  fromCode["22"] = L'U';
+  fromCode["67"] = L'V';
+  fromCode["25"] = L'W';
+  fromCode["68"] = L'X';
+  fromCode["77"] = L'Y';
+  fromCode["44"] = L'Z';
+  fromCode["241"] = L'1';
+  fromCode["238"] = L'2';
+  fromCode["229"] = L'3';
+  fromCode["202"] = L'4';
+  fromCode["121"] = L'5';
+  fromCode["122"] = L'6';
+  fromCode["125"] = L'7';
+  fromCode["134"] = L'8';
+  fromCode["161"] = L'9';
+  fromCode["242"] = L'0';
+  fromCode["355"] = L'-';
+  fromCode["637"] = L'.';
+  fromCode["692"] = L',';
+  fromCode["158"] = L'(';
+  fromCode["644"] = L')';
+  fromCode["400"] = L'?';
+  fromCode["149"] = L'/';*/
+
+  Json::Value toCode;
+
+  keyValueType["keyVariableType"] = "String";
+  keyValueType["valueVariableType"] = "String";
+  toCode["variableType"] = keyValueType;
+  toCode["A"] = ".-";
+  toCode["B"] = "-...";
+  toCode["C"] = "-.-.";
+  toCode["D"] = "-..";
+  toCode["E"] = ".";
+  toCode["F"] = "..-.";
+  toCode["G"] = "--.";
+  toCode["H"] = "....";
+  toCode["I"] = ".---";
+  toCode["J"] = ".---";
+  toCode["K"] = "-.-";
+  toCode["L"] = ".-..";
+  toCode["M"] = "--";
+  toCode["N"] = "-.";
+  toCode["O"] = "---";
+  toCode["P"] = ".--.";
+  toCode["Q"] = "--.-";
+  toCode["R"] = ".-.";
+  toCode["S"] = "...";
+  toCode["T"] = "-";
+  toCode["U"] = "..-";
+  toCode["V"] = "...-";
+  toCode["W"] = ".--";
+  toCode["X"] = "-..-";
+  toCode["Y"] = "-.--";
+  toCode["Z"] = "--..";
+  toCode["1"] = ".----";
+  toCode["2"] = "..---";
+  toCode["3"] = "...--";
+  toCode["4"] = "....-";
+  toCode["5"] = ".....";
+  toCode["6"] = "-....";
+  toCode["7"] = "--...";
+  toCode["8"] = "---..";
+  toCode["9"] = "----.";
+  toCode["0"] = "-----";
+  toCode["-"] = "-....-";
+  toCode["."] = ".-.-.-";
+  toCode[","] = "--..--";
+  toCode["("] = "-.--.";
+  toCode[")"] = "-.--.-";
+  toCode["?"] = "..--..";
+  toCode["/"] = "-..-.";
+  toCode[" "] = "/";
+
+  root["fromCode"] = fromCode;
+  root["toCode"] = toCode;
+
+  // cout << root << endl;
+  Json::StreamWriterBuilder builder;
+  const std::unique_ptr<Json::StreamWriter> writer(builder.newStreamWriter());
+  std::ofstream output_file("MorseKoreanCodeBook.json");
+  writer->write(root, &output_file);
+}
+
+void read_json()
+{
+  std::ifstream input_file("D:/Project/MorseConverter/test.json");
+  std::string rawJson;
+
+  if (input_file.is_open())
+  {
+    input_file.seekg(0, std::ios::end);
+    int size = input_file.tellg();
+    rawJson.resize(size);
+    input_file.seekg(0, std::ios::beg);
+    input_file.read(&rawJson[0], size);
+  }
+  
+  const auto rawJsonLength = static_cast<int>(rawJson.length());
+  constexpr bool shouldUseOldWay = false;
+  JSONCPP_STRING err;
+  Json::Value root;
+
+  if (shouldUseOldWay) {
+    Json::Reader reader;
+    reader.parse(rawJson, root);
+  }
+  else {
+    Json::CharReaderBuilder builder;
+    const std::unique_ptr<Json::CharReader> reader(builder.newCharReader());
+    if (!reader->parse(rawJson.c_str(), rawJson.c_str() + rawJsonLength, &root,
+      &err)) {
+      std::cout << "error" << std::endl;
+      return;
+    }
+  }
+  const int size = root["friends"].size();
+  cout << root["friends"][0]["name"] << endl;
+  // cout << root["friends"]["age"] << endl;
+  //const std::string name = root["name"].asString();
+  //const int age = root["age"].asInt();
+
+  //std::cout << name << std::endl;
+  //std::cout << age << std::endl;
+}
 
 int main()
 {
@@ -41,18 +309,24 @@ int main()
   //wcout << int(L'た') << endl;
   //wcout << int(L'び') << endl;*/
 
+  makeEnglishMorseCodeBook();
+  makeKoreanMorseCodeBook();
 
-  Json::Value root;
-  Json::Value data;
-  root["JsonTest"] = "name";
-  data["try"] = 1;
-  root["data"] = data;
+  // read_json();
 
-	Json::StreamWriterBuilder builder;
-	// const std::string json_file = Json::writeString(builder, root);
-	// std::cout << json_file << std::endl;
-  const std::unique_ptr<Json::StreamWriter> writer(builder.newStreamWriter());
-  writer->write(root, &std::cout);
+  /*Json::Value root;
+  std::ifstream ifs;
+  ifs.open("D:\\Project\\MorseConverter\\test.json");
 
-  return EXIT_SUCCESS;
+  Json::CharReaderBuilder builder;
+  builder["collectComments"] = true;
+  JSONCPP_STRING errs;
+  if (!parseFromStream(builder, ifs, &root, &errs)) {
+    std::cout << errs << std::endl;
+    return EXIT_FAILURE;
+  }
+  std::cout << root << std::endl;
+  return EXIT_SUCCESS;*/
+  
+  return 0;
 }
