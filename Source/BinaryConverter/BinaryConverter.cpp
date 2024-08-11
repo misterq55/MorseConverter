8e1a4577d9b0a77a1182ec4a602827792ed21ad0
규 서문 <illsi@naver.com>
2024-08-11 23:54:28 +0900

    코드 정리

--- Source/BinaryConverter/BinaryConverter.cpp
+++ Source/BinaryConverter/BinaryConverter.cpp
@@ -13,7 +13,6 @@ wstring FBinaryConverter::Encode(const wstring& InString)
 	wstring result = L"";
 
 	wchar_t buffer[100];
-	wchar_t buffer2[100];
 	for (const wchar_t& wch : InString)
 	{
 		memset(buffer, 0, 100 * sizeof(wchar_t));
@@ -41,13 +40,13 @@ wstring FBinaryConverter::Decode(const wstring& InCode)
 	for (const wstring& parsed : parsedCode)
 	{
 		int number = 0;
-		int len = parsed.length();
+		const int len = static_cast<int>(parsed.length());
 
 		for (int i = 0; i < len; i++)
 		{
-			int powered = pow(2, i);
+			const int powered = static_cast<int>(pow(2, i));
 			const wchar_t& wch = parsed[len - i - 1];
-			int num = wch == 0 ? wch : wch - L'0';
+			const int num = wch == 0 ? wch : wch - L'0';
 			number += num * powered;
 		}
 
